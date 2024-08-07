a28283d0e823f767abb6d5684359e2259abbc791
Akbar <akbarqatest@gmail.com>
2024-08-07 19:17:46 +0600

    golf

--- src/main/java/sports/Golf.java
+++ src/main/java/sports/Golf.java
@@ -3,5 +3,8 @@ package sports;
 public class Golf {
     public static void main(String[] args) {
         System.out.println("Golf");
+
+        String str = "Kandai";
+        System.out.println(str);
     }
 }
